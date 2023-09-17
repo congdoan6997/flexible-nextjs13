f5975b6188ab131510dedf4e2fb3a575db56de55
PhDBui <congdoan6997@gmail.com>
2023-09-17 09:23:31 +0700

    create brafbase 2

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -4,8 +4,20 @@ const User = g.model("User", {
   name: g.string().length({min:2, max:20}),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl : g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
 
+})
+
+const Project = g.model("Project", {
+  name: g.string().length({min:3}),
+  description: g.string(),
+  image: g.string(),
+  githubUrl: g.string(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
 })
 
 export default config({
