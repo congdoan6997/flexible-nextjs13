99eb8a3a3a58033dd28c6b6d6b5fb8ad24267bea
PhDBui <congdoan6997@gmail.com>
2023-09-17 08:56:07 +0700

    create brafbase

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,13 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const User = g.model("User", {
+  name: g.string().length({min:2, max:20}),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  
+
+})
+
+export default config({
+  schema: g
+})
